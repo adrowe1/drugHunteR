b4de24d8b03432a7e3f7007691c0d537a7549e38
Alexander D. Rowe <adrowe1@gmail.com>
2016-05-24 17:42:17 +0200

    Starting functions

--- R/additional_functions.R
+++ R/additional_functions.R
@@ -1,34 +1,48 @@
 # Additional functions
 
 
-# tmp -------------------
-#' FIXME
-#' @param fixme a
-#' @return fixme b
+# without function -------------------
+#' Without
+#'
+#' Handy function to remove existing items from a vector - borrowed from the
+#' documentation for  \code{\link{match}}
+#' @param x vector of values
+#' @param y vector of values to be removed from \code{x}
+#' @return vector of remaining values
 #'
 #'
 #' @examples
-#' fixme
-
+#' 1:8 %w/o% c(1,4)
+#'
+#' # character vectors
+#' letters[1:8] %w/o% letters[c(1,4)]
 #' @export
-#' @import dplyr magrittr
 # handy without function
 "%w/o%" <- function(x, y) x[!x %in% y]
 
-# # SQLite query
-# SQLiteQuery <- function(databasePath, databaseName, sqlQuery){
-#   db <- dbConnect(SQLite(), dbname=file.path(databasePath, databaseName))
-#   reply <- dbGetQuery(db, sqlQuery )
-#   dbDisconnect(db)
-#   reply
-# }
 
-# Create link to ChEBI
-createLink <- function(cas, name) {
+
+
+
+# ChEBI link function -----------------
+#' ChEBI link
+#'
+#' Create link to CAS name search in ChEBI
+#'
+#' @param cas CAS reference ID for drug
+#' @param name Molecule name
+#'
+#' @return Link string which can be put into a datatable
+#' @export
+#'
+#' @examples
+#' createChebiLink("drugID", "drugname")
+#'
+createChebiLink <- function(cas, name) {
   sprintf('<a href="http://www.ebi.ac.uk/chebi/advancedSearchFT.do?searchString=%s" target="_blank" class="btn btn-primary">%s</a>',cas, name)
 }
 
-##### END FUNCTIONS ###########
+
 
 
 
